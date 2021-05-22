89d5835f7801b3b80374f71148b782e1c43c5f5f
kalpesh60 <kalpeshdhalpe60@gmail.com>
2021-05-22 23:35:03 +0530

    Added code to read & write Using JsonFile

--- main/java/AddressBookFileIO.java
+++ main/java/AddressBookFileIO.java
@@ -1,3 +1,7 @@
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 import com.opencsv.CSVReader;
 import com.opencsv.CSVWriter;
 import java.io.*;
@@ -5,10 +9,12 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.List;
 
 public class AddressBookFileIO {
 
     private static String fileName = "AddressBook.csv";
+    private static String jsonFileName = "AddressBook.json";
 
     public static boolean readDataFromCSVFile() throws IOException {
         if (Files.exists(Paths.get(fileName))) {
@@ -49,5 +55,33 @@ public class AddressBookFileIO {
                 return false;
             }
         } else return false;
+    }
+
+    public boolean writeIntoJsonFile(List<Contact> contactList) {
+        Gson gson = new Gson();
+        String json = gson.toJson(contactList);
+        try {
+            FileWriter writer = new FileWriter(jsonFileName);
+            writer.write(json);
+            writer.close();
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public boolean readIntoJsonFile(){
+        try {
+            Reader reader = Files.newBufferedReader(Paths.get(jsonFileName));
+            JsonParser jsonParser = new JsonParser();
+            JsonElement obj = jsonParser.parse(reader);
+            JsonArray contactList = (JsonArray) obj;
+            System.out.println(contactList);
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 }
--- test/java/AddressBookIOTest.java
+++ test/java/AddressBookIOTest.java
@@ -2,6 +2,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 public class AddressBookIOTest {
 
@@ -15,10 +16,27 @@ public class AddressBookIOTest {
     @Test
     public void givenCSVFile_WhenWrite_ShouldReturnTrue() {
         ArrayList<String[]> contacts = new ArrayList<>();
-        String[] contact1 = {"Kalpesh", "dha", "mum", "pan", "Mah", "523001", "8106261925", "Vishnu388@gmail.com"};
+        String[] contact1 = {"Kalpesh", "dha", "mum", "pan", "Mah", "523001", "8106261925", "lkjk388@gmail.com"};
         contacts.add(contact1);
         AddressBookFileIO opencsv=new AddressBookFileIO();
         boolean result = opencsv.writeIntoCSVFile(contacts);
         Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void givenJsonFile_WhenWrite_ShouldReturnTrue() {
+        AddressBookFileIO addressBookFileIO = new AddressBookFileIO();
+        List<Contact> contactList = new ArrayList<>();
+        Contact contact1 = new Contact("Kalpesh", "dha", "mum", "pan", "Mah", "523001", "8106261925", "lkjk388@gmail.com");
+        contactList.add(contact1);
+        boolean b = addressBookFileIO.writeIntoJsonFile(contactList);
+        Assertions.assertTrue(b);
+    }
+
+    @Test
+    public void givenJsonFile_WhenRead_ShouldReturnTrue() {
+        AddressBookFileIO addressBookFileIO = new AddressBookFileIO();
+        boolean b = addressBookFileIO.readIntoJsonFile();
+        Assertions.assertTrue(b);
     }
 }
